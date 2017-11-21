af26819589269b92b842641dc48f53f55cbb173f
Sergey <kravchenko.me@gmail.com>
2017-11-21 17:24:30 +0300

    2е задание

---
+++ js/stat.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var chartHeight = 150;
+var barWidth = 40;
+var barIndent = 50;
+var initialX = 120;
+var initialY = 90;
+
+var getMaxArray = function (array) {
+  var max = array[0];
+  for (var i = 1; i <= array.length - 1; i++) {
+    if (max < array[i]) {
+      max = array[i];
+    }
+  }
+
+  return max;
+};
+
+function getRandomNum(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+var getGameResults = function (ctx, names, times) {
+  var step = -chartHeight / (getMaxArray(times) - 0);
+
+  for (var i = 0; i < times.length; i++) {
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + getRandomNum(0.1, 0.9) + ')';
+    }
+
+    var startBarX = initialX + (barWidth + barIndent) * i;
+    var startBarY = initialY + chartHeight;
+    var barHeight = times[i] * step;
+
+    ctx.fillRect(startBarX, startBarY, barWidth, barHeight);
+    ctx.fillStyle = '#000';
+    ctx.fillText(names[i], startBarX, startBarY + 15);
+    ctx.fillText(Math.floor(times[i], 0), startBarX, startBarY + barHeight - 5);
+  }
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0,0,0,0.7)';
+  ctx.fillRect(110, 20, 420, 270);
+
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(100, 10, 420, 270);
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx. fillText('Список результатов:', 120, 60);
+
+  getGameResults(ctx, names, times);
+};
