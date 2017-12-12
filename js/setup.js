61748906ffb538896ecced8a8b99ec3d5d29339e
Sergey <kravchenko.me@gmail.com>
2017-12-12 15:13:41 +0300

    разбил setup на модули setup и wizard

--- js/setup.js
+++ js/setup.js
@@ -2,34 +2,9 @@
 
 window.setup = (function () {
   var wizardSetup = document.querySelector('.setup');
-  var wizardSimilarTemplate = document.getElementById('similar-wizard-template').content.querySelector('.setup-similar-item');
-  var wizardSimilarBlock = document.querySelector('.setup-similar');
-  var wizardSimilarList = wizardSimilarBlock.querySelector('.setup-similar-list');
   var wizardCoatHandler = wizardSetup.querySelector('.setup-wizard .wizard-coat');
   var wizardEyesHandler = wizardSetup.querySelector('.setup-wizard .wizard-eyes');
   var wizardFireBallHandler = wizardSetup.querySelector('.setup-fireball-wrap');
-
-  var WIZARD_NAMES = [
-    'Иван',
-    'Хуан Себастьян',
-    'Мария',
-    'Кристоф',
-    'Виктор',
-    'Юлия',
-    'Люпита',
-    'Вашингтон'
-  ];
-
-  var WIZARD_SURNAMES = [
-    'да Марья',
-    'Верон',
-    'Мирабелла',
-    'Вальц',
-    'Онопко',
-    'Топольницкая',
-    'Нионго',
-    'Ирвинг'
-  ];
 
   var WIZARD_COATS = [
     'rgb(101, 137, 164)',
@@ -55,41 +30,6 @@ window.setup = (function () {
     '#e848d5',
     '#e6e848'
   ];
-
-  var wizards = [];
-
-  for (var i = 0; i < 4; i++) {
-    var probability = 0.5;
-    var nameConcat;
-    if (Math.random() >= probability) {
-      nameConcat = window.util.getRandomFrom(WIZARD_NAMES) + ' ' + window.util.getRandomFrom(WIZARD_SURNAMES);
-    } else {
-      nameConcat = window.util.getRandomFrom(WIZARD_SURNAMES) + ' ' + window.util.getRandomFrom(WIZARD_NAMES);
-    }
-    wizards[i] = {
-      name: nameConcat,
-      coatColor: window.util.getRandomFrom(WIZARD_COATS),
-      eyesColor: window.util.getRandomFrom(WIZARD_EYES)
-    };
-  }
-
-  var renderWizard = function (wizard) {
-    var wizardElement = wizardSimilarTemplate.cloneNode(true);
-
-    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
-
-    return wizardElement;
-  };
-
-  var fragment = document.createDocumentFragment();
-  for (var j = 0; j < wizards.length; j++) {
-    fragment.appendChild(renderWizard(wizards[j]));
-    wizardSimilarList.appendChild(fragment);
-  }
-
-  window.util.removeClass(wizardSimilarBlock, 'hidden');
 
   var getRandomColor = function (el, array) {
     if (el.tagName === 'use') {
